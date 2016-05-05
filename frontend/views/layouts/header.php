13f88a24e3e6d325980ff9cbdba66a73443933ef
quyettvq <quyettvq@gmail.com>
2016-05-05 15:52:46 +0700

    Merge origin/master
    Conflicts:
    	frontend/views/layouts/main.php

--- frontend/views/layouts/header.php
+++ frontend/views/layouts/header.php
@@ -43,7 +43,8 @@ if ($this->context->h1 != '') {
                 } else {
             ?>
             <li class="fl<?= $item->isCurrent() ? ' active' : '' ?>">
-                <a href="javascript:void(0)" title="<?= $item->label ?>" onclick="showmenu('<?= $item->key ?>')"><strong><?= $item->label ?></strong></a><span class="line">|</span>
+                <a href="javascript:void(0)" title="<?= $item->label ?>" onclick="showmenu('<?= $item->key ?>')"><strong><?= $item->label ?></strong></a>
+                <span class="line">|</span>
                 <ul style="display:none" id="<?= $item->key ?>" class="list-unstyle clearfix">
                     <?php
                     foreach ($item->getChildren() as $c_item) {
