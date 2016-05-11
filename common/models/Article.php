6f928cb4951f198034baf96f5985167075aca72f
quyettvq <quyettvq@gmail.com>
2016-05-11 17:34:27 +0700

    Merge origin/master
    Conflicts:
    	frontend/views/layouts/main.php

--- common/models/Article.php
+++ common/models/Article.php
@@ -5,11 +5,5 @@ namespace common\models;
 use yii\db\ActiveRecord;
 
 class Article extends MyActiveRecord {
-    public static $image_resizes = [
-        'large' => [340, 340],
-        'medium' => [230, 230],
-        'small' => [120, 120],
-        'tiny' => [60, 60],
-    ];
     
 }
--- common/models/Product.php
+++ common/models/Product.php
@@ -5,12 +5,4 @@ namespace common\models;
 use yii\db\ActiveRecord;
 
 class Product extends MyActiveRecord {
-    public static $image_resizes = [
-        'large' => [340, 340],
-        'tiny' => [70, 70],
-        'very-tiny' => [60, 60],
-    ];
-    public static $banner_resizes = [
-        'medium' => [340, 340],
-    ];
 }
