a872b341d3b3731718442013710b1369073cf2b9
quyettvq <quyettvq@gmail.com>
2016-05-24 08:52:31 +0700

    Merge origin/master
    Conflicts:
    	frontend/views/layouts/main.php

--- common/config/main.php
+++ common/config/main.php
@@ -12,6 +12,11 @@ return [
             'username' => 'xemtuvi_user',
             'password' => '3XLrQLzc5sXtjzVc',
             'charset' => 'utf8',
+            'enableSchemaCache' => false,
+            // Duration of schema cache.
+            'schemaCacheDuration' => 3600,
+            // Name of the cache component used to store schema information
+            'schemaCache' => 'cache',
         ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
