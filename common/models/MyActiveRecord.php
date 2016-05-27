bfb54a361c0734950ec92fa53bfd3b0cea8f6e07
quyettvq <quyettvq@gmail.com>
2016-05-27 12:20:07 +0700

    Merge origin/master
    Conflicts:
    	frontend/views/layouts/main.php

--- common/models/MyActiveRecord.php
+++ common/models/MyActiveRecord.php
@@ -167,7 +167,7 @@ class MyActiveRecord extends ActiveRecord {
         $length = strlen($content);
         
         for ($i = 1; $i <= $number; $i++) {
-            $start = (int) floor(($length / ($number + 1)) * $i);
+            $start = (int) floor(($length / $number) * ($i - 0.75));
             $pos = strpos($content, $str_find, $start);
             $content = substr_replace($content, "$str_find $adsense", $pos, strlen($str_find));
         }
