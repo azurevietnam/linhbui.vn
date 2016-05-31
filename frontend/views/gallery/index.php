01bbd0017ec278ab65c6e5b511481efac7aaeba8
quyettvq <quyettvq@gmail.com>
2016-05-31 12:04:44 +0700

    Merge origin/master
    Conflicts:
    	frontend/views/layouts/main.php

--- frontend/views/gallery/index.php
+++ frontend/views/gallery/index.php
@@ -15,29 +15,27 @@
         <?= $this->render('//modules/breadcrumbs') ?>
     </div>
     <div class="col-9">
-        <div class="ss-container">
-                    <?= $this->render('//modules/slideshow-multi', [
-                        'data' => $slideshow,
-                        'options' => [
-                            'box_width_preview' => $this->context->is_mobile ? '75%' : '100%',
-                            'img_max_height_preview' => '150px',
-                            'img_wph_ratio_preview' => 1,
-                            'img_margin_preview' => '2%',
-                            'img_number_preview' => $this->context->is_mobile ? 1 : 3,
-                            
-                            'box_width' => $this->context->is_mobile ? '100%' : '60%',
-                            'img_max_width' => '',
-                            'img_max_height' => '100vh',
-                            'img_wph_ratio' => 2,
-                            
-                            'time_slide' => 300,
-                            'time_out' => 3000,
-                            'auto_run' => true,
-                            'pause_on_hover' => true
-                        ]
-                    ]);
-                    ?>
-    </div>
+        <?= $this->render('//modules/slideshow-multi', [
+            'data' => $slideshow,
+            'options' => [
+                'box_width_preview' => $this->context->is_mobile ? '75%' : '100%',
+                'img_max_height_preview' => '150px',
+                'img_wph_ratio_preview' => 1,
+                'img_margin_preview' => '2%',
+                'img_number_preview' => $this->context->is_mobile ? 1 : 3,
+
+                'box_width' => $this->context->is_mobile ? '100%' : '60%',
+                'img_max_width' => '',
+                'img_max_height' => '100vh',
+                'img_wph_ratio' => 2,
+
+                'time_slide' => 300,
+                'time_out' => 3000,
+                'auto_run' => true,
+                'pause_on_hover' => true
+            ]
+        ]);
+        ?>
         <div class="paragraph col-12">
             <h2><?= $model->name ?></h2>
             <?= $model->description ?>
@@ -45,8 +43,3 @@
         <?= $this->render('//modules/comment') ?>
     </div>
 </div>
-<style>
-/*    .ss-container > * {
-        max-height: 300px;
-    }*/
-</style>
